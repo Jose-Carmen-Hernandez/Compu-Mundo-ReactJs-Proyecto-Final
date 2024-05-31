f2c53b719704a65cd3367c9d50d09823a96d0bef
Jose Carmen Hernandez <jcarmenh156@gmail.com>
2024-05-31 15:14:59 -0600

    variables de entorno

--- src/main.jsx
+++ src/main.jsx
@@ -8,14 +8,15 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+// Your web app's Firebase configuration.
+//Guardamos las credenciales de firebase en variables de entorno .env :
 const firebaseConfig = {
-  apiKey: "AIzaSyBmnCIN-WpIw4uKj3lHkdNfdV31PQ9N9Rs",
-  authDomain: "compumundo-react.firebaseapp.com",
-  projectId: "compumundo-react",
-  storageBucket: "compumundo-react.appspot.com",
-  messagingSenderId: "933380735217",
-  appId: "1:933380735217:web:e5cb2071ed282cd6f1e950",
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
